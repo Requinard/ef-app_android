2c9a73cfa1bbade6d760efab298e305a7da33e4c
requinard <d.diks94@gmail.com>
2017-07-09 20:09:17 +0200

    feat(router): add router that supports basic fragment changes
    A router for fragments! We try to hook URL's into the android app, so we can simplify navigation a
    great deal.

---
+++ app/src/main/kotlin/org/eurofurence/connavigator/ui/RouterActivity.kt
@@ -0,0 +1,81 @@
+package org.eurofurence.connavigator.ui
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.*
+import java.util.*
+import kotlin.reflect.full.createInstance
+
+/**
+ * Created by requinard on 7/9/17.
+ */
+class RouterActivity : AppCompatActivity(), AnkoLogger {
+    val routes = mapOf(
+            "/" to FragmentViewHome::class,
+            "/info" to FragmentViewInfoGroups::class,
+            "/event" to FragmentViewEvents::class,
+            "/event/{uid}" to FragmentViewEvent::class
+    )
+
+    val history = Stack<Pair<String, Fragment>>()
+    var current: Pair<String, Fragment>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        info { "Creating router" }
+
+        verticalLayout {
+            button("Events") {
+                setOnClickListener { push("/event") }
+            }
+            button("Info"){
+                setOnClickListener { push("/info") }
+            }
+            id = 1
+        }
+
+        info { "Sending router to default activity" }
+
+        push("/")
+    }
+
+    fun push(route: String) {
+        info { "Pushing route $route" }
+        val fragment = routes[route]
+        if (fragment != null) {
+            info { "Route is valid. Creating instance" }
+
+            // Since we are moving to a new element, we move current to the history
+            history.push(current)
+
+            // then we re-assign current
+            val instance = fragment.createInstance()
+            current = Pair(route, instance)
+
+            // And now we do transaction replacement
+            info { "Starting fragment transaction" }
+
+            supportFragmentManager.beginTransaction()
+                    .replace(1, instance)
+                    .commitAllowingStateLoss()
+        } else {
+            warn { "Supplied route was not valid" }
+        }
+    }
+
+    override fun onBackPressed() {
+        info {"Navigating router back in history"}
+
+        if(history.peek() != null){
+            val lastHistory = history.pop()
+
+            supportFragmentManager.beginTransaction()
+                    .replace(1, lastHistory.second)
+                    .commit()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
